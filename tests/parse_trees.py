003506387cdc9c0fb98b98f35c6dc1c2617accf5
cs187staff <dengyuntian@g.harvard.edu>
2020-10-29 05:05:46 +0000

    automatic update

--- tests/parse_trees.py
+++ tests/parse_trees.py
@@ -1,1 +1,7 @@
-test = {'name': 'parse_trees', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'parse_trees',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> (type(tree1) is nltk.Tree) and (type(tree2) is nltk.Tree) and (tree1!=tree2)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
--- tests/table_addition_rest_back.py
+++ tests/table_addition_rest_back.py
@@ -1,10 +1,9 @@
-test = {   'name': 'table_addition_rest',
+test = {   'name': 'table_addition_rest_back',
     'points': 1,
-    'suites': [   {   'cases': [   {   'code': '>>> # All table elements are empty or string;\n'
-                                               '>>> functools.reduce(lambda x, y: x and y,\n'
-                                               '...                  [all([type(item) is str for item in table.iloc[i, j]])\n'
+    'suites': [   {   'cases': [   {   'code': '>>> functools.reduce(lambda x, y: x and y,\n'
+                                               '...                  [all([type(item) is tuple for item in back.iloc[i, j].values()])\n'
                                                '...                   for i, j in [(i0, j0) for i0 in range(5) for j0 in range(i0+1, 5)]]) \n'
-                                               'True',
+                                               'False',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
